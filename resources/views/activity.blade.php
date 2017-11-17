e5871d0b7de916d09a2fb7e721f6664719018912
maiotsai <s1041515@gm.pu.edu.tw>
2017-11-17 17:17:52 +0800

    在activity加入header

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+@extends('header')
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -11,15 +7,18 @@
       margin-top: 200px;
     }
   </style>
-</head>
 
 <title>活動</title>
 
 <body>
+@section('header')
+    
+@stop
+
+@section('content')
 
 <div class="w3-container w3-center w3-animate-opacity">
   Comming Soon
 </div>
-
+@stop
 </body>
-</html>
