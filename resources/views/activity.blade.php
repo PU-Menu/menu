213c274b69cf36b319c9319a381800bda48af96d
maiotsai <s1041515@gm.pu.edu.tw>
2017-11-18 22:48:05 +0800

    修改activity

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -3,10 +3,62 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
   <style>
-    .content{
-      margin-top: 200px;
-      font-size: 50px;
-    }
+  .content{
+    margin-top: 100px;
+    font-size: 50px;
+  }
+
+  .content2{
+    font-size: 30px;
+  }
+
+  .button {
+  display: inline-block;
+  border-radius: 4px;
+  border: none;
+  color: black;
+  background-color: white;
+  text-align: center;
+  font-size: 28px;
+  padding: 10px;
+  width: 180px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+.article{
+  margin-top: 50px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  opacity: 0.9;
+  border-radius: 10px;
+}
   </style>
 
 <title>活動</title>
@@ -18,8 +70,37 @@
 
 @section('content')
 
-<div class="w3-container w3-center w3-animate-opacity content">
-  Comming Soon
-</div>
+<div class="py-5">
+    <div class="w3-container w3-center w3-animate-opacity content">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>今日主題：櫟舍文學餐廳介紹</h1><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="w3-container w3-center w3-animate-opacity content2">
+      <div class="row">
+        <div class="col-md-12">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/CxyhanBrK0k" frameborder="0" allowfullscreen></iframe><p><br>
+          耕讀分享 x 季節款待<p>
+          <br>
+          <br>
+          透過料理，領略作家的精神和經歷；藉由廚房，探訪臺中人的生活樣貌──這是「櫟舍文學餐廳」的重要初衷。<p>
+　        <br>
+          <br>
+          「櫟舍文學餐廳」坐落「臺中文學館」內，原為 1932 年完工的日本警察宿舍，2009 年登錄歷史建築，隔年進行古蹟修繕工程；如今，飄散淡雅檜木味的日式建築正臥於百年老榕寬厚的樹傘下，安穩又扎實的，與柳川和第五市場為鄰。<p>
+　        <br>
+          <br>
+          於 2017 年 8 月 15 日開放試營運，提供文學餐（第一季為散文類型餐、陳千武文學主題餐、楊逵野菜鹹派）、小山園抹茶等飲品及冰品；同時，舉辦作家廚房、作家午茶、文學沙龍、茶道課、花道課等活動，希望這棟老屋仍有人依照它原本的樣子在作息。<p>
+        </div>
+      </div>
+    </div>
+  </div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
 @stop
 </body>
