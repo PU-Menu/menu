019df031b6f989f6ae7b0d8da9a593e80ad8c349
a410414970b <s1041497@gm.pu.edu.tw>
2017-12-12 10:41:30 +0800

    add:footer在Author

--- resources/views/Author.blade.php
+++ resources/views/Author.blade.php
@@ -66,10 +66,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
-<footer>
-  <h5 class="text-right">
-    資料來源：http://www.tlm.taichung.gov.tw/form/index.aspx?Parser=2,8,46
-  </h5>
-</footer>
+@stop
+@section('footer')    
 @stop 
+
+ 
 </html>
