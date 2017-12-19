3602c47ccca9db97430579126b745ea7fd19a307
yjwu <s1041535@gm.pu.edu.tw>
2017-12-19 15:39:54 +0800

    fixed: header 增加about us資訊

--- resources/views/Author.blade.php
+++ resources/views/Author.blade.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">
-
 <style>
 
 .display-1{
@@ -15,6 +14,11 @@
     top:50px;
     font-size: 84px;
 }
+
+body{
+  background-color:#292b2c;
+}
+
 </style>
 </head>
 
