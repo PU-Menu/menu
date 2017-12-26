da130977cca4e350acd6ee4e5216ba3f816cc4a6
maiotsai <s1041515@gm.pu.edu.tw>
2017-12-26 13:07:12 +0800

    fixed: index 修改背景 activity 修改div排版

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,12 +22,13 @@
 </head>
 
 <style>
+
 .button {
   display: inline-block;
-  border-radius: 4px;
-  background-color: grey;
+  border-radius: 10px;
+  background-color: #CD853F;
   border: none;
-  color: white;
+  color: black;
   text-align: center;
   font-size: 28px;
   padding: 10px;
@@ -63,19 +64,13 @@
 }
 
 .article{
-  padding-top: 10px;
-  padding-bottom: 10px;
+  margin-top: 30px;
   opacity: 0.9;
-  margin-top: 10px;
-  margin-bottom: 30px;
-  border-radius: 10px;
-  font-size: 30px;
+  border-radius: 5px;
+  font-size: 40px;
 }
-
-.article2{
-  margin-top: 20px;
-  opacity: 0.9;
-  border-radius: 10px;
+body{
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
 }
 
 </style>
@@ -84,21 +79,18 @@
 
   <!-- Page Content -->
   
-    <div class="container w3-center">
+    <div class="container w3-display-middle">
       <div class="row">
-        <div class="col-md-12 w3-light-grey w3-animate-opacity article2">
-          <h1 class="mt-5">「文學食旅，無所味懼」</h1>
+        <div class="col-md-3">
+          
         </div>
-
-        <div class="col-md-12 w3-large w3-light-grey w3-animate-opacity article">
-
-          <p>臺中自日治時期即為臺灣文壇的中心，從傳統文學社團櫟社、臺灣文社，開風氣之先，引領全臺，到新文學組織臺灣文藝聯盟及其相關文學刊物，乃至林獻堂所領導的臺灣文化協會，都以臺中為主要活動據點，而市區的中央書局不但是當時文學家、藝術家活動的根據地，也是新文化的傳播站；延續到戰後，臺中則是銀鈴會、笠詩社等本土文學社團的發跡地。以上史實，充分彰顯臺中作為臺灣文學發展中心的光榮傳統與重要地位，「文化城」之美名可謂其來有自。</p>
- 
-          <p>臺中文學館原為日治時期的警察宿舍，1932年落成。基於歷史建築之永續保存，呈現時代的精神與脈絡，記錄大臺中地區文學發展的軌跡，彰顯在地文學家的成就，臺中市政府文化局於2009年將其登錄為歷史建築，以「臺中文學館」為定位，2010年4月進行修復與活化再利用工程。館舍於2015年4月外觀修復完成，先行開放文學公園，同時進行內部展示工程的設計、佈置，2016年8月26日各館舍全面開放。</p>
-
-          <p>臺中文學館全園區佔地約1,863坪，館舍共分6棟，包含常設展區、主題展區、兒童文學區、研習講堂、主題餐飲區及行政區。各館舍以展覽、研習、推廣為主要用途，透過多元活潑的展示手法及互動體驗，達到文學教育與文化休閒之目的。館舍因深具建築美學與歷史氛圍，已成為文化觀光的新亮點。</p>
+        <div class="col-md-6 w3-center w3-animate-opacity article">
+          文學食旅，無所<b>味</b>懼 
+          <h2>櫟社文學餐廳</h2>
         </div>
-
+        <div class="col-md-3">
+          
+        </div>
       </div>
     </div>
 
